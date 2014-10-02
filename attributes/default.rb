d4f29b9c7cecc01497597656754729f47bdce331
Martin Halamíček <martin@halamicek.cz>
2014-10-02 16:27:22 +0200

    Init

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+
+default['php']['packages'] = %w{ php54 php54-devel php-pear php54-pdo php54-mysql php54-mbstring php54-pgsql php54-mcrypt php54-pecl-apc }
+default['php']['directives'] = {
+    'date.timezone' => 'Europe/Prague'
+}
